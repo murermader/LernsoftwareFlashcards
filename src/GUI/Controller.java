5a8d65a3eb327cf1f931d3bf127795147a0f1318
KaiTroendle <kai.troendle@web.de>
2019-01-25 13:51:52 +0100

    GUI hinzugefügt

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,5 +1,33 @@
 package GUI;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
+public class Controller {
+  //View
+    public Button ueben;
+    public Button ansehen;
+    public   Button hinzufuegen;
+    public Button beenden;
+
+    @FXML
+    public void handlerUeben(){
+
+    }
+
+    @FXML
+    public  void handlerAnsehen(){
+
+    }
+
+    @FXML
+    public  void handlerHinzufuegen(){
+
+    }
+
+    @FXML
+    public void handlerBeenden(){
+
+    }
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -17,8 +17,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GUIWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Flashcards");
+        primaryStage.setScene(new Scene(root, 400, 500));
         primaryStage.show();
     }
 
