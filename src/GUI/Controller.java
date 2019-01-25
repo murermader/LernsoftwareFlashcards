6d582580cfa38107873e427fd99be42402a99829
KaiTroendle <kai.troendle@web.de>
2019-01-25 15:04:42 +0100

    Zu den neuen Windows wechseln können

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,33 +1,84 @@
 package GUI;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import GUI.Main;
+
+import java.io.IOException;
 
 public class Controller {
   //View
-    public Button ueben;
-    public Button ansehen;
-    public   Button hinzufuegen;
-    public Button beenden;
+    public Button handlerUeben;
+    public Button handlerAnsehen;
+    public Button handlerHinzufuegen;
+    public Button handlerBeenden;
 
+    private Main main;
+
+    public void setMain(Main main){
+      this.main = main;
+    }
     @FXML
-    public void handlerUeben(){
-      System.out.println("Handler Üben");
+    public void handlerUeben()throws Exception{
+
+      FXMLLoader loader = new FXMLLoader(Main.class.getResource("UebenWindow.fxml"));
+      AnchorPane pane = loader.load();
+
+      Scene scene = new Scene(pane);
+
+
+      Stage primaryStage = new Stage();
+      primaryStage.setTitle("Flashcards");
+      primaryStage.setScene(scene);
+      primaryStage.show();
     }
 
     @FXML
-    public  void handlerAnsehen(){
+    public  void handlerAnsehen()throws Exception{
       System.out.println("Handler Ansehen");
+      FXMLLoader loader = new FXMLLoader(Main.class.getResource("UebenWindow.fxml"));
+      AnchorPane pane = loader.load();
+
+      Scene scene = new Scene(pane);
+
+
+      Stage thridStage = new Stage();
+      thridStage.setTitle("Flashcards");
+      thridStage.setScene(scene);
+      thridStage.show();
     }
 
     @FXML
-    public  void handlerHinzufuegen(){
+    public  void handlerHinzufuegen()throws Exception{
       System.out.println("Handler Hinzufügen");
+      FXMLLoader loader = new FXMLLoader(Main.class.getResource("UebenWindow.fxml"));
+      AnchorPane pane = loader.load();
+
+      Scene scene = new Scene(pane);
+
+
+      Stage fourthStage = new Stage();
+      fourthStage.setTitle("Flashcards");
+      fourthStage.setScene(scene);
+      fourthStage.show();
     }
 
     @FXML
-    public void handlerBeenden(){
+    public void handlerBeenden()throws Exception{
       System.out.println("Handler Beenden");
+      FXMLLoader loader = new FXMLLoader(Main.class.getResource("UebenWindow.fxml"));
+      AnchorPane pane = loader.load();
+
+      Scene scene = new Scene(pane);
+
+
+      Stage fifthStage = new Stage();
+      fifthStage.setTitle("Flashcards");
+      fifthStage.setScene(scene);
+      fifthStage.show();
     }
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import javafx.application.Application;
@@ -14,14 +15,44 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Stage primaryStage;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("GUIWindow.fxml"));
-        primaryStage.setTitle("Flashcards");
-        primaryStage.setScene(new Scene(root, 400, 500));
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        mainWindow(primaryStage);
     }
 
+    public void mainWindow(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("GUIWindow.fxml"));
+        AnchorPane pane = loader.load();
+
+        Scene scene = new Scene(pane);
+
+        Controller controller = loader.getController();
+        controller.setMain(this);
+
+        primaryStage.setTitle("Flashcards");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+
+    public void UebenWindow(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("UebenWindow.fxml"));
+        AnchorPane pane = loader.load();
+
+        Scene scene = new Scene(pane);
+
+        Controller controller = loader.getController();
+        controller.setMain(this);
+
+        Stage secondaryStage = new Stage();
+        secondaryStage.setTitle("Flashcards");
+        secondaryStage.setScene(scene);
+        secondaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
