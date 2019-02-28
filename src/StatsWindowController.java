9a0b5ac9c662dae2d5eefab29ca763f6fb9540ef
silmurer <37251616+silmurer@users.noreply.github.com>
2019-03-01 00:07:56 +0100

    Statistik View hinzugefügt

--- src/StatsWindowController.java
+++ src/StatsWindowController.java
@@ -1,13 +1,57 @@
 import java.io.IOException;
+import java.util.logging.Level;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class StatsWindowController {
+
+    //FXML Elemente
+    public Label nameLable = new Label();
+    public Label cardCount = new Label();
+    public Label deckCount = new Label();
+
+    public Button allUserSwitch = new Button();
+    public Button timeSwitch = new Button();
+
+    public Helper helper = new Helper();
+    public Data data = new Data();
+    public User user = new User(data.getCurrentUser());
+
+    //Globale Variabeln
+    private String name;
+    private int numberOfDecks;
+    private int numberOfCards;
+    private int cardsLearned;
+    private long timeSpentLearning;
+
+    @FXML
+    public void initialize() {
+        user.setName(data.getCurrentUser());
+        user.setNumberOfDecks(data.getListOfDecks().size());
+    //    user.setNumberOfCards();
+    //    user.setCardsLearned();
+    //    user.setTimeSpentLearning();
+
+        name = user.getName();
+        numberOfDecks = user.getNumberOfDecks();
+
+        nameLable.setText(name);
+        deckCount.setText("" + user.getNumberOfDecks());
+    }
+
+    public void handlerAllUser(ActionEvent event) throws IOException {
+
+
+    }
+
 
     public void handlerBack(ActionEvent event) throws IOException {
 
