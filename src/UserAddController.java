6ef1170d7ab0919ff13f434c36f45bfb0e3e1161
murermader <biehler.rafael@gmail.com>
2019-03-02 19:06:15 +0100

    UserAdd: Statusbar hinzugefügt, mit Info

--- src/UserAddController.java
+++ src/UserAddController.java
@@ -5,17 +5,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
+import javafx.scene.control.Label;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
 public class UserAddController {
 
     public TextField textField = new TextField();
+    public Label statusBarLabel1 = new Label();
     private Helper helper = new Helper();
-
 
     public void handlerConfirm(ActionEvent event) throws IOException {
 
@@ -31,23 +30,23 @@ public class UserAddController {
                     LogHelper.writeToLog(Level.INFO, "Username konnte nicht angenommen werden, da der Name " + user + " schon existiert.");
                 }
             }
-            if (!newUser.equals("Beispieldeck") && newUser.length() < 30 && userNameIsUnique) {
+            if (!newUser.equals("Beispieldeck") && newUser.length() < 30 && newUser.length() > 2 && userNameIsUnique) {
                 allUsers.add(newUser);
+                helper.saveUsersToFile(allUsers);
+                switchToManageUserView(event);
+            } else {
+                statusBarLabel1.setText("Info: Der Name muss länger als 2 und kürzer als 30 Zeichen sein.");
             }
         }
-        helper.saveUsersToFile(allUsers);
-
-        Parent manageUserView = FXMLLoader.load(getClass().getResource("GUI/manageUser.fxml"));
-        Scene practiceViewScene = new Scene(manageUserView);
-        Stage window1 = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window1.setScene(practiceViewScene);
-        window1.show();
     }
 
     public void handlerBack(ActionEvent event) throws IOException {
+        switchToManageUserView(event);
+    }
+
+    private void switchToManageUserView(ActionEvent event) throws IOException {
         Parent manageUserView = FXMLLoader.load(getClass().getResource("GUI/manageUser.fxml"));
         Scene practiceViewScene = new Scene(manageUserView);
-        //This line gets the Stage information
         Stage window1 = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window1.setScene(practiceViewScene);
         window1.show();
