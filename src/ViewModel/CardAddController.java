c3f973317ac764742302e5fa780b5eca577c8b00
murermader <biehler.rafael@gmail.com>
2019-03-03 18:08:51 +0100

    CardAddController: Man kann jetzt Karten hinzufügen + Statusbar

--- src/ViewModel/CardAddController.java
+++ src/ViewModel/CardAddController.java
@@ -1,34 +1,69 @@
 package ViewModel;
 
-import Model.Data;
-import Model.Helper;
+import Model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.util.logging.Level;
 
 public class CardAddController {
 
+    public HBox statusbar = new HBox();
+    public Label statusbarLabel1 = new Label();
+    public TextField questionTextField = new TextField();
+    public TextField answerTextField = new TextField();
+    public Button addButton = new Button();
 
-    public ListView CardList = new ListView();
+    private Helper helper = new Helper();
+    private Data data = new Data();
+    private Deck deck;
 
-    Helper helper = new Helper();
-    Data data = new Data();
 
     @FXML
     public void initialize() {
+        try{
 
+            statusbar.setBackground(new Background(new BackgroundFill(Color.rgb(212, 212, 212), CornerRadii.EMPTY, Insets.EMPTY)));
+
+            if(!Data.getCurrentUser().isEmpty()){
+                deck = data.getCurrentDeck();
+                statusbarLabel1.setText("Aktuelles Deck: " + deck.getName());
+
+            } else {
+                statusbarLabel1.setText("Aufgrund eines Fehlers konnte der jetzige Benutzer nicht ermittelt werden");
+                addButton.setDisable(true);
+            }
+        } catch (Exception ex){
+            LogHelper.writeToLog(Level.INFO, "Beim Initialisieren des CardAddControllers kam es zu einem Fehler: " +ex);
+        }
     }
 
     @FXML
     public void handlerBack(ActionEvent event) throws IOException {
-        helper.switchScene(event, "MainWindow.fxml");
+        helper.switchScene(event, "CardOverview.fxml");
     }
 
     @FXML
     public void handlerAdd(ActionEvent event) throws IOException {
-        System.out.println(Data.getCurrentDeckName());
-        //TODO: Hier fertigmachen
+
+        if(!questionTextField.getText().isEmpty() && !answerTextField.getText().isEmpty()){
+
+            Flashcard flashcard = new Flashcard(questionTextField.getText(), answerTextField.getText());
+            deck.addCard(flashcard);
+            helper.saveDeckToFile(deck);
+            statusbarLabel1.setText("Karte hinzugefügt.");
+        } else {
+            statusbarLabel1.setText("Karte konnte nicht hinzugefügt werden. (Leere Felder?)");
+        }
     }
 }
